ea6cad452a5da8de27ec92d3ed662282e1a7f36b
松岡侑幹 <yuki@yukinoMacBook-Air.local>
2023-05-06 12:37:59 +0900

    second

--- urls.py
+++ urls.py
@@ -27,7 +27,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("http://35.213.92.81/admin/", admin.site.urls),
     path("flower/", include("flower.urls")),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
